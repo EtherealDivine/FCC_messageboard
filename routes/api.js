3422229ce56045bb32e4bb9671d71b8d7145dfc6
Divine <divine@Divines-MacBook-Pro.local>
2024-06-16 17:30:13 +0000

    vscode

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,58 @@
-'use strict';
+"use strict";
+let mongodb = require("mongodb");
+let mongoose = require("mongoose");
 
 module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+  let uri = process.env.MONGO_URI;
+  console.log("Connecting to MongoDB with URI:", uri);
 
+  mongoose
+    .connect(uri)
+    .then(() => console.log("MongoDB connected successfully"))
+    .catch((err) => console.log("MongoDB connection error:", err));
+
+  let replySchema = new mongoose.Schema({
+    text: { type: String, required: true },
+    delete_password: { type: String, required: true },
+    createdon_: { type: Date, required: true },
+    reported: { type: Boolean, required: true },
+  });
+
+  let threadSchema = new mongoose.Schema({
+    text: { type: String, required: true },
+    delete_password: { type: String, required: true },
+    board: { type: String, required: true },
+    createdon_: { type: Date, required: true },
+    bumpedon_: { type: Date, required: true },
+    reported: { type: Boolean, required: true },
+    replies: [replySchema],
+  });
+
+  let Reply = mongoose.model("Reply", replySchema);
+  let Thread = mongoose.model("Thread", threadSchema);
+
+  app.post("/api/threads/:board", async (request, response) => {
+    let newThread = new Thread(request.body);
+    if (!newThread.board || newThread.board === "") {
+      newThread.board = request.params.board;
+    }
+    newThread.createdon_ = new Date();
+    newThread.bumpedon_ = new Date();
+    newThread.reported = false;
+    newThread.replies = [];
+
+    try {
+      let savedThread = await newThread.save();
+      console.log("Thread saved successfully:", savedThread);
+      const baseUrl = `${request.protocol}://${request.get("host")}`;
+      const redirectUrl = `${baseUrl}/b/${savedThread.board}/${savedThread._id}`;
+      console.log("Redirecting to:", redirectUrl);
+      response.redirect(redirectUrl);
+    } catch (error) {
+      console.log("Error saving thread:", error);
+      response.status(500).send("Error saving thread");
+    }
+  });
+
+  // Other routes can be added here
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,30 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite("Functional Tests", function () {
+  let testThreadId;
+  let testReplyId;
+  let testPass = "testpass";
 
+  test("Create a New Thread", (done) => {
+    chai
+      .request(server)
+      .post("/api/threads/test")
+      .send({
+        board: "test",
+        text: "Functional Test Thread",
+        delete_password: testPass,
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        let createdThreadId =
+          res.redirects[0].split("/")[res.redirects[0].split("/").length - 1];
+        testThreadId = createdThreadId;
+        done();
+      });
+  });
 });
